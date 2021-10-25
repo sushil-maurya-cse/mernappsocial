a2a27cb7684d1432a5fb15dae0a0e90859e91038
Sushil Kumar <sushil.kumar@infostride.com>
2021-10-25 21:43:56 +0530

    remover path

--- index.js
+++ index.js
@@ -59,9 +59,9 @@ app.post("/posing", async (req, res) => {
 if ( process.env.NODE_ENV == "production")
 { app.use(express.static("client/build")); 
 const path = require("path");
- app.get("*", (req, res) =>{ 
+ /* app.get("*", (req, res) =>{ 
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
-  })
+  }) */
 }
 
 
